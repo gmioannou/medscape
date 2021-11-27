c52d5ca4286dc674556122f34362b7ce43c7042e
George Ioannou <gmioannou@gmail.com>
2021-11-27 12:25:47 +0200

    update layers

--- app/src/views/MapView.vue
+++ app/src/views/MapView.vue
@@ -6,10 +6,12 @@
 
 <script>
 import {Map, View} from 'ol'
-import TileLayer from "ol/layer/Tile";
+// import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
 import { fromLonLat } from "ol/proj";
 import TileWMS from 'ol/source/TileWMS';
+import ImageWMS from 'ol/source/ImageWMS';
+import {Image as ImageLayer, Tile as TileLayer} from 'ol/layer';
 
 // http://localhost:8081/geoserver/medscape/wms?service=WMS&version=1.1.0&request=GetMap&layers=medscape%3ACyprus_LVL2&bbox=432298.625%2C3823859.0%2C644787.6875%2C3952037.75&width=768&height=463&srs=EPSG%3A32636&styles=&format=application/openlayers
 export default {
@@ -33,9 +35,18 @@ export default {
     let admb_dist = new TileLayer({
       source: new TileWMS({
         url: `${geoserverUrl}/medscape/wms?`,
-        params: {'LAYERS': 'medscape:admb_dist', 'TILED': true},
+        params: {'LAYERS': 'medscape:admb_dist'},
         serverType: 'geoserver',
         transition: 0,
+      }),
+    });
+
+    let admb_dist_image = new ImageLayer({
+      source: new ImageWMS({
+        url: `${geoserverUrl}/medscape/wms?`,
+        params: {'LAYERS': 'medscape:admb_dist'},
+        ratio: 1,
+        serverType: 'geoserver',
       }),
     });
 
@@ -48,14 +59,23 @@ export default {
       }),
     });
 
+    let medscape_polygons_image = new ImageLayer({
+      source: new ImageWMS({
+        url: `${geoserverUrl}/medscape/wms?`,
+        params: {'LAYERS': 'medscape:Cyprus_LVL2'},
+        ratio: 1,
+        serverType: 'geoserver',
+      }),
+    });
+
     this.olmap = new Map({
       target: 'ol-map',
       layers: [
         new TileLayer({
           source: new OSM(),
         }),
-        medscape_polygons,
-        admb_dist
+        medscape_polygons_image,
+        admb_dist_image
       ],
       view: new View({
         center: fromLonLat([33.3587, 35.17025]),
